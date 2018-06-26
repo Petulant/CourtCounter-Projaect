569ceaa8f22b9b7bbbe58d0b9763409b628cbf01
Thembelihle <pmokhwebane@gmail.com>
2018-06-26 15:38:50 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,53 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  // variable assign globally
 
+  scoreA=0;
+  scoreB=0;
+
+  
   constructor(public navCtrl: NavController) {
 
   }
 
+  
+  onclickA(){
+    this.scoreA +=3;
+    console.log(this.scoreA);
+  
+  }
+  onclickAA(){
+    this.scoreB +=3;
+    console.log(this.scoreB);
+  
+  }
+
+  onclickB(){
+      this.scoreA +=2;
+      console.log(this.scoreA);
+    
+  }
+  onclickBB(){
+    this.scoreB +=2;
+    console.log(this.scoreB);
+  }
+
+  onclickC(){
+    this.scoreA +=1;
+    console.log(this.scoreA);
+    
+    }
+    onclickCC(){
+      this.scoreB +=1;
+      console.log(this.scoreB);
+    }
+      
+
+    Reset(){
+      this.scoreA=0;
+      this.scoreB=0;
+
+    }
+      
 }
